8fdd26e264f2bb92fcea755443a5cdccd57cc9ab
Pat Howeson <pat@howeson.co.uk>
2018-03-19 20:59:37 +0000

    Initial commit

---
+++ CodeKatas.Tests.Unit/IsTriangleTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace CodeKatas.Tests.Unit
+{
+    [TestFixture]
+    public class IsTriangleTests
+    {
+        [Test]
+        public void IsTriangle_ValidPostiveNumbers_ReturnsTrue()
+        {
+            Assert.IsTrue(TriangleKata.IsTriangle(5, 7, 10));
+        }
+
+        [TestCase(1, 2, 3)]
+        [TestCase(1, 3, 2)]
+        [TestCase(3, 1, 2)]
+        public void IsTriangle_SumOfSidesEqualOtherSide_ReturnsFalse(int a, int b, int c)
+        {
+            Assert.IsFalse(TriangleKata.IsTriangle(a, b, c));
+        }
+
+        [TestCase(-1, 2, 3)]
+        [TestCase(1, -2, 3)]
+        [TestCase(1, 2, -3)]
+        public void IsTriangle_NegativeSides_ReturnsFalse(int a, int b, int c)
+        {
+            Assert.IsFalse(TriangleKata.IsTriangle(a, b, c));
+        }
+
+        [TestCase(1, 2, 4)]
+        [TestCase(4, 1, 2)]
+        [TestCase(1, 4, 2)]
+        public void IsTriangle_PositiveSides_ReturnsFalse(int a, int b, int c)
+        {
+            Assert.IsFalse(TriangleKata.IsTriangle(a, b, c));
+        }
+    }
+}
---
+++ CodeKatas/TriangleKata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas
+{
+    public class TriangleKata
+    {
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            return !(a <= 0 || b <= 0 || c <= 0) && (a + b > c && b + c > a && c + a > b);
+        }
+    }
+}
